1e7b332da310f1b79de52adb5c36132d9f9b6160
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-11-30 12:52:31 -0500

    set folder and file structure for app per readme instructions

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Bacon cheeseburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Veggie burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Chicken burger', true);
+
