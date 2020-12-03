b2e8a449ffb3f1dd214d1196d16dcf582ede2586
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-12-03 12:14:33 -0500

    updated controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -46,6 +46,19 @@ router.put("/api/burgers/:id", function (req, res) {
       }
     }
   );
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 // Export routes for server.js to use.
